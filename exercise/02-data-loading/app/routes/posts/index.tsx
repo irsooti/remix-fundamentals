98fc03674737faef02efe2cc2de03b97bbc8e4c4
Daniele Irsuti <irsooti+code@gmail.com>
2022-11-05 17:42:23 +0100

    data loading

--- exercise/02-data-loading/app/routes/posts/index.tsx
+++ exercise/02-data-loading/app/routes/posts/index.tsx
@@ -1,7 +1,29 @@
+import { useLoaderData } from "@remix-run/react";
+import type { DataFunctionArgs } from "@remix-run/server-runtime";
+import { json } from "@remix-run/server-runtime";
+import { Link } from "react-router-dom";
+import { getPosts } from "~/models/posts.server";
+
+export const loader = async (_: DataFunctionArgs) => {
+  const data = await getPosts();
+
+  return json(data);
+};
+
 export default function Posts() {
+  const posts = useLoaderData<typeof loader>();
   return (
     <main>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
