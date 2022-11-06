5b660fe7a352d0264caa9a4c8363cf7ebee626a2
Daniele Irsuti <irsooti+code@gmail.com>
2022-11-06 02:41:18 +0100

    multiple forms

--- exercise/07-multiple-forms/app/routes/posts/admin/$slug.tsx
+++ exercise/07-multiple-forms/app/routes/posts/admin/$slug.tsx
@@ -9,7 +9,12 @@ import {
 import invariant from "tiny-invariant";
 
 // 🐨 you'll need to import `deletePost` and `updatePost` here as well.
-import { createPost, getPost } from "~/models/post.server";
+import {
+  createPost,
+  deletePost,
+  getPost,
+  updatePost,
+} from "~/models/post.server";
 
 export async function loader({ params }: LoaderArgs) {
   invariant(params.slug, "slug not found");
@@ -23,34 +28,59 @@ export async function loader({ params }: LoaderArgs) {
 }
 
 // 🐨 you'll need the `params` in the action
-export async function action({ request }: ActionArgs) {
+export async function action({ request, params }: ActionArgs) {
   const formData = await request.formData();
   // 🐨 grab the "intent" from the form data
+  const intent = formData.get("intent");
+  const slug = formData.get("slug");
+  invariant(params.slug, "slug not found");
 
-  // 🐨 if the intent is "delete" then delete the post
-  // and redirect to "/posts/admin"
+  console.log({ intent, slug });
+
+  if (intent === "delete") {
+    // 🐨 call `deletePost` here
+    deletePost(params.slug);
+    return redirect("/posts/admin");
+  }
 
   const title = formData.get("title");
-  const slug = formData.get("slug");
   const markdown = formData.get("markdown");
 
-  const errors = {
+  const errors: any = {
     title: title ? null : "Title is required",
-    slug: slug ? null : "Slug is required",
     markdown: markdown ? null : "Markdown is required",
   };
+
+  if (intent === "create") {
+    errors.slug = slug ? null : "Slug is required";
+  }
+
   const hasErrors = Object.values(errors).some((errorMessage) => errorMessage);
   if (hasErrors) {
     return json(errors);
   }
 
   invariant(typeof title === "string", "title must be a string");
-  invariant(typeof slug === "string", "slug must be a string");
   invariant(typeof markdown === "string", "markdown must be a string");
 
   // 🐨 if the params.slug is "new" then create a new post
   // otherwise update the post.
-  await createPost({ title, slug, markdown });
+
+  if (intent === "create") {
+    invariant(typeof slug === "string", "slug must be a string");
+
+    await createPost({
+      title,
+      slug,
+      markdown,
+    });
+  } else {
+    await updatePost({
+      title,
+      slug: params.slug,
+      markdown,
+    });
+  }
 
   return redirect("/posts/admin");
 }
@@ -66,9 +96,18 @@ export default function PostAdmin() {
   // we'll need to disambiguate between them. You can do that with
   // the "intent" in the form data.
   // 💰 transition.submission?.formData.get("intent")
-  const isCreating = Boolean(transition.submission);
+  const isCreating = Boolean(
+    transition.submission?.formData.get("intent") === "create",
+  );
+  const isDeleting = Boolean(
+    transition.submission?.formData.get("intent") === "delete",
+  );
+  const isUpdating = Boolean(
+    transition.submission?.formData.get("intent") === "update",
+  );
   // 🐨 create an isUpdating and isDeleting variable based on the transition
   // 🐨 create an isNewPost variable based on whether there's a post on `data`.
+  const isNewPost = !data.post;
 
   return (
     <Form method="post">
@@ -120,20 +159,31 @@ export default function PostAdmin() {
           defaultValue={data?.post?.markdown}
         />
       </p>
-      {/* 🐨 If we're editing an existing post, then render a delete button */}
-      {/* 💰 The button's "name" prop should be "intent" and the "value" prop should be "delete" */}
-      {/* 💰 Here's some good looking classes for it: className="rounded bg-red-500 py-2 px-4 text-white hover:bg-red-600 focus:bg-red-400 disabled:bg-red-300" */}
+      <button
+        className="px-2 py-1 text-white bg-red-500 rounded hover:bg-red-600"
+        name="intent"
+        value="delete"
+      >
+        {isDeleting ? "Deleting..." : "Delete"}
+      </button>
       {/* 🐨 It should say "Deleting..." when a submission with the intent "delete" is ongoing, and "Delete" otherwise. */}
       <p className="text-right">
         <button
           type="submit"
           // 🐨 add a name of "intent" and a value of "create" if this is a new post or "update" if it's an existing post
-          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          name="intent"
+          value={isNewPost ? "create" : "update"}
+          className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
           // 🐨 this should be disabled if we're creating *or* updating
           disabled={isCreating}
         >
-          {/* 🐨 if this is a new post then this works fine as-is, but if we're updating it should say "Updating..." / "Update" */}
-          {isCreating ? "Creating..." : "Create Post"}
+          {!isNewPost
+            ? isUpdating
+              ? "Updating..."
+              : "Update"
+            : isCreating
+            ? "Creating..."
+            : "Create Post"}
         </button>
       </p>
     </Form>
