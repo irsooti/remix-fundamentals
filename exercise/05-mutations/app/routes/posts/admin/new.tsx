f8ed696dc3a8c0d61e47a2e51afcc569a3bc381e
Daniele Irsuti <irsooti+code@gmail.com>
2022-11-06 01:16:23 +0100

    forms

--- exercise/05-mutations/app/routes/posts/admin/new.tsx
+++ exercise/05-mutations/app/routes/posts/admin/new.tsx
@@ -5,13 +5,40 @@
 // 4. call the createPost function from your post.model.ts
 // 5. redirect to "/posts/admin".
 
+import { Form, useActionData } from "@remix-run/react";
+import { ActionArgs, json } from "@remix-run/server-runtime";
+import { redirect } from "@remix-run/server-runtime";
+import { createPost } from "~/models/post.server";
+
+export async function action({ request }: ActionArgs) {
+  const formData = await request.formData();
+  const title = formData.get("title") as string;
+  const slug = formData.get("slug") as string;
+  const markdown = formData.get("markdown") as string;
+
+  if (
+    typeof title === "string" &&
+    title.length > 3 &&
+    typeof slug === "string" &&
+    typeof markdown === "string"
+  ) {
+    await createPost({ title, slug, markdown });
+    return redirect("/posts/admin");
+  }
+
+  return json({ error: "Invalid form data" }, { status: 400 });
+}
+
 const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
 
 export default function NewPost() {
+  const actionData = useActionData<typeof action>();
+
   return (
     // 🐨 change this to a <Form /> component from @remix-run/react
     // 🐨 and add method="post" to the form.
-    <form>
+
+    <Form method="post">
       <p>
         <label>
           Post Title:{" "}
@@ -37,11 +64,12 @@ export default function NewPost() {
       <p className="text-right">
         <button
           type="submit"
-          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
         >
           Create Post
         </button>
+        {actionData?.error}
       </p>
-    </form>
+    </Form>
   );
 }
